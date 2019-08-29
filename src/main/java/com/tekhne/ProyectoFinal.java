116ede39545bda9eba6487a98e788a3556d94e94
Ariel Dayler Salazar Hinojosa <adayler@gmail.com>
2019-08-29 00:02:15 -0400

    Minor comments

--- src/main/java/com/tekhne/ProyectoFinal.java
+++ src/main/java/com/tekhne/ProyectoFinal.java
@@ -27,13 +27,15 @@ public class ProyectoFinal {
             
             genteRepo.agregar(new Persona(482721, "Roberto", "Gomez"));
             genteRepo.actualizar(164594, new Persona(164594, "Peter", "Gabriel"));
-
+            // Always check if the RS has the required structure
             genteRepo.executeQuery(Persona.class, "SELECT * FROM personas WHERE apellido LIKE 'G%'")
-                    .map(p -> p.getId()).forEach(System.out::println);
+                     .map(p -> p.getId())
+                     .forEach(System.out::println);
             
             System.out.println("****");
+            // For the primitive types just check if the first column has a compatible type
             genteRepo.executeQuery(String.class, "SELECT nombre FROM personas")
-                    .forEach(System.out::println);
+                     .forEach(System.out::println);
             
             System.out.println("****");
             genteRepo.delete(164592);
